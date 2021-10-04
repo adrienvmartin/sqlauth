a41a6fcaf17490da41154f2f02fd74326d9872a4
Adrien Martin <adrien.v.martin@gmail.com>
2021-10-03 20:36:35 -0600

    Add error check for MySQL connection

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const mysql = require('mysql');
 const app = express();
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'localhost:3306',
   user: 'root',
   password: '',
+  database: 'node_login'
 });
+
+db.connect((error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('MySQL connected!')
+  }
+})
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
