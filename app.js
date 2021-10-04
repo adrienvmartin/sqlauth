95c8b695c87c9c10ae6b1d8f585a45b92553fab0
Adrien Martin <adrien.v.martin@gmail.com>
2021-10-03 20:13:53 -0600

    Initialize MySQL connection

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
